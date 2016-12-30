ab67873bd5196a47fb7b3928d2194384e1d3ad67
zhangzju <zhangway19921221@163.com>
2016-12-31 02:41:29 +0800

    add inert

--- server.js
+++ server.js
@@ -3,7 +3,23 @@
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: 3001 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
 
 server.start((err) => {
 
