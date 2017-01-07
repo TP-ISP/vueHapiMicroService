68aeb2a526b41da40d1c9a955c7d6b2f9697538c
zhangzju <zhangway19921221@163.com>
2017-01-07 23:19:13 +0800

    add static files

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Path = require('path');
 
-const server = new Hapi.Server();
-server.connection({ port: 3001 });
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'dist')
+            }
+        }
     }
 });
+server.connection({ port: 3001 });
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
     }
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./index.html');
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/build.js',
+        handler: function (request, reply) {
+            reply.file('./build.js');
+        }
+    });
 });
 
 server.start((err) => {
